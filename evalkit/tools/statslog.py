bf507ee2b1e774621e3de5a518b72a207400f9fc
parvector <parvector@yandex.com>
2021-05-03 19:29:31 +0700

    Added logging to the file. Task 8 in the roadmap.

--- evalkit/tools/statslog.py
+++ evalkit/tools/statslog.py
@@ -4,7 +4,7 @@ from functools import reduce
 
 
 class StatsLog:
-    def __init__(self, funcs:list, spaces="  ", viznt_funcs=[]):
+    def __init__(self, funcs:list, spaces="  ", viznt_funcs=[], path=None):
         """
         Arguments:
         :funcs = <list>
@@ -13,14 +13,15 @@ class StatsLog:
             Margins in a row, between columns of statistical information that the write function returns.
         :non_viz_funcs = <list>
             Contains a list of functions whose results are recorded in log, but not displayed on the screen.
-        #TODO
-        :format = "json" or "csv"
         :path = <str>
-        Path for writing statistical information to a csv or json file.
+            Path for writing statistical information to a csv or json file.
         """
         self.funcs = funcs
         self.viznt_funcs = viznt_funcs
         self.spaces = spaces
+        self.__path = path
+        if self.__path != None:
+            self.__doc = open(self.__path, "w")
         self.headers = self.__create_headers()
         self.log = {}
         self.__gen = 0
@@ -40,7 +41,7 @@ class StatsLog:
         self.__gen += 1
         self.log[self.__gen] = {}
         #calculate value of funcs
-        for func in [self.funcs, self.viznt_funcs]:
+        for func in self.funcs + self.viznt_funcs:
             self.log[self.__gen][f"{func.__name__}"] = func( pop )
 
         #create string for return
@@ -48,12 +49,18 @@ class StatsLog:
         for key in self.log[self.__gen]:
             if key not in [ func.__name__ for func in self.viznt_funcs ]:
                 ret_str += str(self.log[self.__gen][key])+"|"+self.spaces
-            
+        
+        #write string to doc
+        self.__write_doc(string=ret_str)
         return ret_str
+
+    def __write_doc(self, string):
+        self.__doc.write(string+"\n")
         
     def __create_headers(self):
         str_headers = "gen" + "|" + self.spaces
         for func in self.funcs:
             if func.__name__ not in [ func.__name__ for func in self.viznt_funcs ]:
                 str_headers += func.__name__ + "|" + self.spaces
+        self.__doc.write(str_headers+"\n")
         return str_headers
